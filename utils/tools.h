9ece99c9baeb181d28ea2d92a1d6344e190dca61
HuaiyanRen <100552380+HuaiyanRen@users.noreply.github.com>
2025-07-14 10:10:47 +1000

    draft about mAIC for merging

--- utils/tools.h
+++ utils/tools.h
@@ -1029,6 +1029,9 @@ public:
 
     /** use logarithm of rates for clustering algorithm */
     bool partfinder_log_rate;
+
+    /** use mAIC as partition merging criterion */
+    bool marginal_lh_aic;
     
     /************************************************/
     /******* variables for Terrace analysis *********/
